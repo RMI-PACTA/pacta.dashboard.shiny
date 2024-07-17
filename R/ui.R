6fda30cb7e2b52c7be29ed79386905595f6617eb
Alex Axthelm <aaxthelm@RMI.org>
2024-07-17 14:12:33 +0200

    namespace UI elements

--- R/ui.R
+++ R/ui.R
@@ -1,16 +1,16 @@
 # Modified from https://rstudio.github.io/bslib/articles/dashboards/index.html
-ui <- page_sidebar(
+ui <- bslib::page_sidebar(
   title = "mtcars dashboard",
-  sidebar = sidebar(
+  sidebar = bslib::sidebar(
     title = "Histogram controls",
-    varSelectInput(
+    shiny::varSelectInput(
       "var", "Select variable",
       dplyr::select_if(mtcars, is.numeric)
     ),
-    numericInput("bins", "Number of bins", 30L)
+    shiny::numericInput("bins", "Number of bins", 30L)
   ),
-  card(
-    card_header("Histogram"),
-    plotOutput("p")
+  bslib::card(
+    bslib::card_header("Histogram"),
+    shiny::plotOutput("p")
   )
 )
