81647d8fc152c6d1786ef002e166fd4c0dc04c68
Alex Axthelm <aaxthelm@RMI.org>
2024-07-17 16:11:31 +0200

    Use bslib for intro page

--- R/page_intro.R
+++ R/page_intro.R
@@ -1,24 +1,20 @@
-page_intro <- shiny::tags[["html"]](
-  shiny::tags[["head"]](
-    shiny::tags[["title"]]("PACTA Introduction")
+page_intro <- bslib::page_fillable(
+  title = "PACTA Introduction",
+  htmltools::p("This is the PACTA Intro page."),
+  htmltools::p("PACTA is love"),
+  htmltools::p("PACTA is life"),
+  htmltools::h1("PACTA is a Dashboard"),
+  htmltools::p(
+    "Here is some sample text.",
+    "We can use the normal formatting, such as",
+    htmltools::strong("bold"),
+    htmltools::em("italic"),
+    "and",
+    htmltools::code("code"),
+    " text."
   ),
-  shiny::tags[["body"]](
-    htmltools::p("This is the PACTA Intro page."),
-    htmltools::p("PACTA is love"),
-    htmltools::p("PACTA is life"),
-    htmltools::h1("PACTA is a Dashboard"),
-    htmltools::p(
-      "Here is some sample text.",
-      "We can use the normal formatting, such as",
-      htmltools::strong("bold"),
-      htmltools::em("italic"),
-      "and",
-      htmltools::code("code"),
-      " text."
-    ),
-    htmltools::div(
-      id = "myDiv", class = "simpleDiv",
-      "Here is a div with some attributes."
-    )
+  htmltools::div(
+    id = "myDiv", class = "simpleDiv",
+    "Here is a div with some attributes."
   )
 )
