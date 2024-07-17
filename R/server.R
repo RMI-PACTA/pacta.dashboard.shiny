e316a12913cf1c4f2e88cc75d1e130a2706135ee
Alex Axthelm <aaxthelm@RMI.org>
2024-07-17 15:08:52 +0200

    Reduce to static server

--- R/server.R
+++ R/server.R
@@ -1,11 +1,3 @@
 # https://github.com/rstudio/shiny-examples/blob/main/083-front-page/server.R
 server <- function(input, output) {
-  output[["p"]] <- shiny::renderPlot({
-    ggplot2::ggplot(mtcars) +
-      ggplot2::geom_histogram(
-        ggplot2::aes(!!input[["var"]]),
-        bins = input[["bins"]]
-      ) +
-      ggplot2::theme_bw(base_size = 20L)
-  })
 }
