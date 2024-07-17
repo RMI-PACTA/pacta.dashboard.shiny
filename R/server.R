bd3a6a87e37c846481b27c962c62a1708ad2e677
Alex Axthelm <aaxthelm@RMI.org>
2024-07-17 14:05:39 +0200

    Use bslib template

--- R/server.R
+++ R/server.R
@@ -1,30 +1,11 @@
 # https://github.com/rstudio/shiny-examples/blob/main/083-front-page/server.R
 server <- function(input, output) {
-
-  output[["main_plot"]] <- shiny::renderPlot({
-    log_debug("Rendering main plot")
-
-    hist(
-      faithful[["eruptions"]],
-      probability = TRUE,
-      breaks = as.numeric(input[["n_breaks"]]),
-      xlab = "Duration (minutes)",
-      main = "Geyser eruption duration"
-    )
-
-    if (input[["individual_obs"]]) {
-      logger::log_trace("Adding individual observations")
-      rug(faithful[["eruptions"]])
-    }
-
-    if (input[["density"]]) {
-      logger::log_trace("Adding density estimate")
-      dens <- density(
-        faithful[["eruptions"]],
-        adjust = input[["bw_adjust"]]
-      )
-      lines(dens, col = "blue")
-    }
-
+  output[["p"]] <- shiny::renderPlot({
+    ggplot2::ggplot(mtcars) +
+      ggplot2::geom_histogram(
+        ggplot2::aes(!!input[["var"]]),
+        bins = input[["bins"]]
+      ) +
+      ggplot2::theme_bw(base_size = 20L)
   })
 }
--- R/ui.R
+++ R/ui.R
@@ -1,35 +1,16 @@
-# https://github.com/rstudio/shiny-examples/blob/main/083-front-page/ui.R
-ui <- bootstrapPage(
-
-  selectInput(
-    inputId = "n_breaks",
-    label = "Number of bins in histogram (approximate):",
-    choices = c(10L, 20L, 35L, 50L),
-    selected = 20L
+# Modified from https://rstudio.github.io/bslib/articles/dashboards/index.html
+ui <- page_sidebar(
+  title = "mtcars dashboard",
+  sidebar = sidebar(
+    title = "Histogram controls",
+    varSelectInput(
+      "var", "Select variable",
+      dplyr::select_if(mtcars, is.numeric)
+    ),
+    numericInput("bins", "Number of bins", 30L)
   ),
-
-  checkboxInput(
-    inputId = "individual_obs",
-    label = strong("Show individual observations"),
-    value = FALSE
-  ),
-
-  checkboxInput(
-    inputId = "density",
-    label = strong("Show density estimate"),
-    value = FALSE
-  ),
-
-  plotOutput(outputId = "main_plot", height = "300px"),
-
-  # Display this only if the density is shown
-  conditionalPanel(
-    condition = "input.density == true",
-    sliderInput(
-      inputId = "bw_adjust",
-      label = "Bandwidth adjustment:",
-      min = 0.2, max = 2L, value = 1L, step = 0.2
-    )
+  card(
+    card_header("Histogram"),
+    plotOutput("p")
   )
-
 )
