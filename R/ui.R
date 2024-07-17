206846ae315310a971443ca3141abe273ba7f03e
Alex Axthelm <aaxthelm@RMI.org>
2024-07-17 13:51:22 +0200

    linting

--- R/ui.R
+++ R/ui.R
@@ -1,26 +1,35 @@
 # https://github.com/rstudio/shiny-examples/blob/main/083-front-page/ui.R
 ui <- bootstrapPage(
 
-  selectInput(inputId = "n_breaks",
-      label = "Number of bins in histogram (approximate):",
-      choices = c(10, 20, 35, 50),
-      selected = 20),
+  selectInput(
+    inputId = "n_breaks",
+    label = "Number of bins in histogram (approximate):",
+    choices = c(10L, 20L, 35L, 50L),
+    selected = 20L
+  ),
 
-  checkboxInput(inputId = "individual_obs",
-      label = strong("Show individual observations"),
-      value = FALSE),
+  checkboxInput(
+    inputId = "individual_obs",
+    label = strong("Show individual observations"),
+    value = FALSE
+  ),
 
-  checkboxInput(inputId = "density",
-      label = strong("Show density estimate"),
-      value = FALSE),
+  checkboxInput(
+    inputId = "density",
+    label = strong("Show density estimate"),
+    value = FALSE
+  ),
 
   plotOutput(outputId = "main_plot", height = "300px"),
 
   # Display this only if the density is shown
-  conditionalPanel(condition = "input.density == true",
-    sliderInput(inputId = "bw_adjust",
-        label = "Bandwidth adjustment:",
-        min = 0.2, max = 2, value = 1, step = 0.2)
+  conditionalPanel(
+    condition = "input.density == true",
+    sliderInput(
+      inputId = "bw_adjust",
+      label = "Bandwidth adjustment:",
+      min = 0.2, max = 2L, value = 1L, step = 0.2
+    )
   )
 
 )
